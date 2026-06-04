b311135aa164b88ec5db7f21b6e551d2aeeb898e
Naren S K <sknaren786@gmail.com>
2026-06-04 11:04:23 +0530

    prepared for deployment

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,10 @@ import cors from "cors";
 import connectDB from "./config/connectDB.js";
 import router from "./routes/authRoutes.js";
 import songRouter from "./routes/songRoutes.js";
-
+import path from "path";
 dotenv.config(".env");
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -15,13 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 // Connect yout Database
 connectDB();
 // cors- cross origin resource sharing
-app.use(
-  cors({
-    // origin: "*" - all can acess backend
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      // origin: "*" - all can acess backend
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
+}
 
 // cors- cross origin resource sharing, cant share resources of backend without permission
 // backened is running on port 5000 and 5173 is frontend
@@ -35,5 +38,12 @@ app.use(
 // payload- data of user that u want t share
 app.use("/api/songs", songRouter);
 app.use("/api/auth", router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 // CORS error
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
